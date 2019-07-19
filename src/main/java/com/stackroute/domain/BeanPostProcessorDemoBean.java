dc7b109e0032a0c12e3c08c8a607d6ed17a62f1e
sankeerthana1762 <51943073+sankeerthana1762@users.noreply.github.com>
2019-07-19 11:27:32 +0530

    Update BeanPostProcessorDemoBean.java
    comments added

--- src/main/java/com/stackroute/domain/BeanPostProcessorDemoBean.java
+++ src/main/java/com/stackroute/domain/BeanPostProcessorDemoBean.java
@@ -4,7 +4,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
 public class BeanPostProcessorDemoBean implements BeanPostProcessor {
-
+    // implementing methods of BeanPostProcessor Interface 
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         System.out.println("Inside postProcessBeforeInitialization method");
