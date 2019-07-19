73e3df5afe41d4e5db04986e053dc3bda1866991
sankeerthana1762 <51943073+sankeerthana1762@users.noreply.github.com>
2019-07-19 11:03:23 +0530

    Update Main.java
    Added comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,10 +6,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main( String[] args )
     {
+        //Using ApplicationContext
         ApplicationContext context = new AnnotationConfigApplicationContext(ConfigurationClass.class);
         Movie movie1 = context.getBean("movieBean",Movie.class);
         movie1.display();
-
+       //using BeanFactory
         BeanFactory factory = new AnnotationConfigApplicationContext(ConfigurationClass.class);
         Movie movie2 = factory.getBean("movieBean",Movie.class);
         movie2.display();
